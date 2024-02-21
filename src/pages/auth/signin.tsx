df301f3caa390cf28e515572301ecd19321d5cbb
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-02-21 00:00:17 -0500

    login styles

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -3,25 +3,31 @@ import type {
     InferGetServerSidePropsType,
   } from "next"
 import { getCsrfToken } from "next-auth/react"
-
+import "../../app/globals.css"
 export default function SignIn({
     csrfToken,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
-        <form method="post" action="/api/auth/callback/credentials">
-        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-        <label>
-            Username
-        </label>
-        <label>
-            Email
-            <input name="email" type="email" />
-        </label>
-        <label>
-            Password
-            <input name="password" type="password" />
-        </label>
-        <button type="submit">Sign in</button>
+        <form method="post" action="/api/auth/callback/credentials" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+            
+            <div className="mb-4">
+                <label className="block text-orange-700 text-sm font-bold mb-2">
+                    Email
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" />
+            </div>
+            <div className="mb-6">
+                <label className="block text-orange-700 text-sm font-bold mb-2">
+                    Password
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" />
+            </div>
+            <div className="flex items-center justify-between">
+                <button className="primary-color hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Sign in
+                </button>
+            </div>
         </form>
     )
 }
