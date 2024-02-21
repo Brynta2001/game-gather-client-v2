82918f385c23ce5e1458bd51b0de5aafa1c924b8
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-02-21 18:56:30 -0500

    feat: email send notification

--- src/components/forms/RecoverForm.tsx
+++ src/components/forms/RecoverForm.tsx
@@ -11,6 +11,9 @@ interface EmailData {
 
 
 const PasswordRecoveryForm: React.FC = () => {
+
+    const [buttonState, setButtonState] = React.useState(false);
+
     const initialValues = {
         email: '',
     };
@@ -26,46 +29,50 @@ const PasswordRecoveryForm: React.FC = () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            }).catch((error) => {} );            
+            }).catch((error) => {} );
+            setButtonState(true);
+            alert('Password recovery email sent, check your email!');            
         } catch (error) {
             console.error(error);
         }
+        
+        };
+
+        return (
+            <div className="flex justify-center items-center">
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        <FormTitle title="Password Recovery" />
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <Field
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="Enter your email"
+                            />
+                            <ErrorMessage name="email" component="div" className="text-red-500 text-xs mt-1" />
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <button
+                                className="primary-color hover:bg-orange-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="submit"
+                                disabled={buttonState}
+                            >
+                                Recover Password
+                            </button>
+                        </div>
+                    </Form>
+                </Formik>
+            </div>
+        );
     };
 
-    return (
-        <div className="flex justify-center items-center">
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={handleSubmit}
-            >
-                <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <FormTitle title="Password Recovery" />
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                            Email
-                        </label>
-                        <Field
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="email"
-                            type="email"
-                            name="email"
-                            placeholder="Enter your email"
-                        />
-                        <ErrorMessage name="email" component="div" className="text-red-500 text-xs mt-1" />
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <button
-                            className="primary-color hover:bg-orange-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="submit"
-                        >
-                            Recover Password
-                        </button>
-                    </div>
-                </Form>
-            </Formik>
-        </div>
-    );
-};
-
-export default PasswordRecoveryForm;
+    export default PasswordRecoveryForm;
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,7 @@ export { default } from "next-auth/middleware";
  
 export const config = {
   matcher: [
-    '/dashboard:path*',
-    '/account-activated:path*',
+    '/dashboard:path*',       
     '/reset-password:path*',
   ],
 }
