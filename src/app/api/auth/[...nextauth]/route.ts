2c4f7bb0dc871a37a4c61b22a3ea1409b8c5ab29
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2024-02-20 23:39:04 -0500

    feat: add middleware for protecting routes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -50,5 +50,4 @@ export const authOptions: NextAuthOptions = {
 }
 
 const handler = NextAuth(authOptions);
-export default handler;
-//export { handler as GET, handler as POST }
+export { handler as GET, handler as POST }
