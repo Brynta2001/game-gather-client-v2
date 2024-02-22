dfa9e308ef37befe3c3f5c7eb8d46c2fd28c21ca
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-02-21 20:02:54 -0500

    fix: accound activated redirect

--- src/components/others/AccountActivated.tsx
+++ src/components/others/AccountActivated.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AccountActivated: React.FC = () => {
@@ -8,9 +9,9 @@ const AccountActivated: React.FC = () => {
             </svg>
             <h1 className="text-2xl font-bold text-orange-500 mb-2">Account Activated</h1>
             <p className="text-sm mb-4">Thank you for verifying your account.</p>
-            <a href="/login" className="black_btn">
+            <Link href="/auth/signin" className="black_btn">
                 Click here to login
-            </a>
+            </Link>
         </div>
     );
 };
