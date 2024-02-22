e5e20bcb0c9d124bac4fb693254de0c152d979b0
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-02-21 20:33:54 -0500

    fix: account-activated

--- src/app/account-activated/page.tsx
+++ src/app/account-activated/page.tsx
@@ -1,43 +1,18 @@
 'use client'
 import React, { Suspense, useEffect, useState } from 'react';
-import AccountActivated from '@/components/others/AccountActivated';
-import { axiosInstance } from '@/lib/axios-instance';
 import { useSearchParams } from 'next/navigation'
 import ErrorMessage from '@/components/others/ErrorMessage';
-import { useRouter } from 'next/navigation';
+import VerifyActivation from '@/components/others/VerifyActivation';
 
 const Page: React.FC = () => {
     const searchParams = useSearchParams()
-    const [activated, setActivated] = useState(false);
-    const router = useRouter();
-
+    const [activated, setActivated] = useState(false);      
     
-    useEffect(() => {
-        const activateAccount = async () => {
-            if (searchParams) {
-                const id = searchParams.get('id')
-                const code = searchParams.get('code')
-                const activationData = { id, code }
-
-                await axiosInstance.post('/auth/activate-account', activationData)
-                    .then(response => {
-                        console.log(response.data);
-                        setActivated(true);
-                    })
-                    .catch(error => {
-                        console.error(error.response.data.message);
-                    });
-
-            }
-        }
-        if (searchParams) activateAccount();
-
-    }, [searchParams]);
 
     return (
         <Suspense fallback={<ErrorMessage text='Something went wrong. Please wait or try again later.' />}>
             <div>
-                {activated ? <AccountActivated /> : <ErrorMessage text='Something went wrong. Please wait or try again later.' />}
+                <VerifyActivation />
             </div>
         </Suspense>
     );
---
+++ src/components/others/VerifyActivation.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React, { Suspense, useEffect, useState } from 'react';
+import AccountActivated from '@/components/others/AccountActivated';
+import { axiosInstance } from '@/lib/axios-instance';
+import { useSearchParams } from 'next/navigation'
+import ErrorMessage from '@/components/others/ErrorMessage';
+
+const VerifyActivation: React.FC = () => {
+    const searchParams = useSearchParams()
+    const [activated, setActivated] = useState(false);   
+
+    
+    useEffect(() => {
+        const activateAccount = async () => {
+            if (searchParams) {
+                const id = searchParams.get('id')
+                const code = searchParams.get('code')
+                const activationData = { id, code }
+
+                await axiosInstance.post('/auth/activate-account', activationData)
+                    .then(response => {
+                        console.log(response.data);
+                        setActivated(true);
+                    })
+                    .catch(error => {
+                        console.error(error.response.data.message);
+                    });
+
+            }
+        }
+        if (searchParams) activateAccount();
+
+    }, [searchParams]);
+
+    return (
+        
+            <div>
+                {activated ? <AccountActivated /> : <ErrorMessage text='Something went wrong. Please wait or try again later.' />}
+            </div>
+        
+    );
+};
+
+export default VerifyActivation;
